9734afd773717a4d083f223754897eb86b8679f0
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2017-08-14 12:22:06 +0300

    Why no promisify()?

--- api/src/controllers/secret.js
+++ api/src/controllers/secret.js
@@ -23,6 +23,7 @@ const allowedFields = [
 
 const randomSuf = () => crypto.randomBytes(3).toString('hex');
 
+// util.promisify() won't work on AWS SDK functions
 function assumeRole(roleName, purpose) {
     const prefix = purpose.substring(0, 57).replace(/[^\w+=,.@-]/g, '-');
     const params = {
