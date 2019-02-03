1d0f4d3a92853f056ac333ee0815924dc76ddbab
Arkadi Shishlov <arkadi@agilestacks.com>
2019-02-04 01:33:20 +0200

    Use `ecr` component

--- cloud/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  type = "string"
-  description = "Logical name of the environment to be able co-exist with other environments under the same AWS account or region"
-}
-
-variable "base_domain" {
-  type = "string"
-  description = "common domain name for the stack"
-}
