e9093919471fe820ee445334499f834a53c6f137
Arkadi Shishlov <arkadi@agilestacks.com>
2019-06-11 22:31:37 +0300

    No Dredd tests possible

--- api/test/dreddHooks.js
+++
@@ -1,7 +0,0 @@
-const hooks = require('hooks'); // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
-
-/* eslint no-param-reassign: ["error", { "props": false }] */
-
-hooks.beforeEach((tx) => {
-    if (tx.request.method === 'DELETE') tx.skip = true;
-});
